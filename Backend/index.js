ad7d0550c5f3618c1d725e3638002aa5fc18b44d
Zakaria Beloufa <zakaria.beloufa13@gmail.com>
2024-01-21 17:59:59 +0100

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
 // Enable CORS
-app.use(cors());
+app.use(cors(
+  {
+ origin: ["https://deploy-hostea.vercel.app"],
+ credentials: true,
+ methods: ["GET", "POST", "PUT", "DELETE"]
+}
+));
 //define routes
 app.use("/",userRoute)
 app.use("/",houseRoute)
