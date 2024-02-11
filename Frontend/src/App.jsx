f884840a094a8c5dfb28fea15b9d682e13ddcd1b
Zakaria Beloufa <zakaria.beloufa13@gmail.com>
2024-02-11 13:55:37 +0100

    dashboard modifications

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,3 @@
-// App.js
-
 import "./App.css";
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,6 +7,7 @@ import Logout from "./routes/Logout";
 import Login from "./routes/Login";
 import Header from "./routes/Header";
 import Profile from "./routes/Profile";
+import Dashboard from "./routes/Dashboard";
 import AccommodationDetail from "./routes/AccommodationDetail";
 import ProfileSection from "./routes/ProfileSection"; // Import the ProfileSection component
 import BookingsSection from "./routes/BookingsSection"; // Import the BookingsSection component
@@ -17,41 +16,56 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectUser, logoutUser, getUser } from "./slice/userSlice";
 import { useNavigate } from "react-router-dom";
 
+
 const App = () => {
   const dispatch = useDispatch();
   const userData = useSelector(selectUser);
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState("");
   const handleSearch = (term) => {
     setSearchTerm(term);
   };
   const handleLogout = () => {
     dispatch(logoutUser());
     localStorage.removeItem("token");
-    
   };
   useEffect(() => {
     dispatch(getUser());
-  }, []);
+    console.log(location.pathname)
+  }, [location.pathname]);
 
-  return (<>
-    <Router>
-      <div className="App lg:pl-10 lg:pr-10 overflow-x-hidden ">
-        <Header isLoggedIn={Boolean(userData)} onLogout={handleLogout} setSearchTerm={handleSearch}  className="sticky top-0 z-50"/>
+  return (
+    <>
+      <Router>
+      {location.pathname !== "/dashboard" &&<div className="App lg:pl-10 lg:pr-10 overflow-x-hidden ">
+           <Header
+            isLoggedIn={Boolean(userData)}
+            onLogout={handleLogout}
+            setSearchTerm={handleSearch}
+            className="sticky top-0 z-50"
+          />
+          <Routes>
+            <Route exact path="/" element={<Home searchTerm={searchTerm} />} />
+            <Route path="/:id" element={<AccommodationDetail />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/Form" element={<Form />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/profile" element={<Profile />}>
+              {/* Nested routes for Profile component */}
+              <Route index element={<ProfileSection />} />
+              <Route path="bookings" element={<BookingsSection />} />
+              <Route
+                path="accommodations"
+                element={<AccommodationsSection />}
+              />
+            </Route>
+          </Routes>
+        </div>}
+      </Router>
+      <Router>
         <Routes>
-          <Route exact path="/" element={<Home searchTerm={searchTerm}/>} />
-          <Route path="/:id" element={<AccommodationDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Form" element={<Form />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/profile" element={<Profile />}>
-            {/* Nested routes for Profile component */}
-            <Route index element={<ProfileSection />} />
-            <Route path="bookings" element={<BookingsSection />} />
-            <Route path="accommodations" element={<AccommodationsSection />} />
-          </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
     </>
   );
 };
---
+++ Frontend/src/routes/Dashboard.jsx
@@ -0,0 +1,160 @@
+// Contact.js
+import {
+  MagnifyingGlassIcon,
+  ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+  CardFooter,
+  Tabs,
+  TabsHeader,
+  Tab,
+  Avatar,
+  IconButton,
+  Tooltip,
+} from "@material-tailwind/react";
+const TABS = [
+  {
+    label: "All",
+    value: "all",
+  },
+  {
+    label: "Monitored",
+    value: "monitored",
+  },
+  {
+    label: "Unmonitored",
+    value: "unmonitored",
+  },
+];
+
+const TABLE_HEAD = ["Member", "Function", "Status", "Employed", ""];
+
+const TABLE_ROWS = [
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
+    name: "John Michael",
+    email: "john@creative-tim.com",
+    job: "Manager",
+    org: "Organization",
+    online: true,
+    date: "23/04/18",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
+    name: "Alexa Liras",
+    email: "alexa@creative-tim.com",
+    job: "Programator",
+    org: "Developer",
+    online: false,
+    date: "23/04/18",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
+    name: "Laurent Perrier",
+    email: "laurent@creative-tim.com",
+    job: "Executive",
+    org: "Projects",
+    online: false,
+    date: "19/09/17",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
+    name: "Michael Levi",
+    email: "michael@creative-tim.com",
+    job: "Programator",
+    org: "Developer",
+    online: true,
+    date: "24/12/08",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
+    name: "Richard Gran",
+    email: "richard@creative-tim.com",
+    job: "Manager",
+    org: "Executive",
+    online: false,
+    date: "04/10/21",
+  },
+];
+
+const Dashboard = () => {
+  return (<div>
+    <Card className="h-full w-full overflow-scroll m-8">
+      <table className="w-full min-w-max table-auto text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th
+                key={head}
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+              >
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {TABLE_ROWS.map(({ name, job, date }, index) => (
+            <tr key={name} className="even:bg-blue-gray-50/50">
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {name}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {job}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  {date}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography
+                  as="a"
+                  href="#"
+                  variant="small"
+                  color="blue-gray"
+                  className="font-medium"
+                >
+                  Edit
+                </Typography>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Card>
+    </div>
+  );
+};
+
+export default Dashboard;
